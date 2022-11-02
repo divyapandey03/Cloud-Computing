5ed5fb9346e2a66a5e6cc5dc841edfb0594f2405
Divya Pandey <divya.pandey012430@gmail.com>
2022-11-02 13:34:34 -0700

    Create SparkPageRank.scala

---
+++ Spark/Pagerank/SparkPageRank.scala
@@ -0,0 +1,84 @@
+package org.apache.spark.examples
+
+import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Computes the PageRank of URLs from an input file. Input file should
+ * be in format of:
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * URL         neighbor URL
+ * ...
+ * where URL and their neighbors are separated by space(s).
+ *
+ * This is an example implementation for learning how to use Spark. For more conventional use,
+ * please refer to org.apache.spark.graphx.lib.PageRank
+ */
+object SparkPageRank {
+
+  def showWarning() {
+    System.err.println(
+      """WARN: This is a naive implementation of PageRank and is given as an example!
+        |Please use the PageRank implementation found in org.apache.spark.graphx.lib.PageRank
+        |for more conventional use.
+      """.stripMargin)
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: SparkPageRank <file> <iter>")
+      System.exit(1)
+    }
+
+    showWarning()
+
+    val sparkConf = new SparkConf().setAppName("PageRank")
+    val iters = if (args.length > 0) args(1).toInt else 10
+    val ctx = new SparkContext(sparkConf)
+    val lines = ctx.textFile(args(0), 1)
+    // Input file:
+    //   B  C
+    //   C  B
+    //   E  B
+    //   E  D
+    //   E  F
+    //   D  A
+    //   D  B
+    //   F  B
+    //   F  E
+    // Output links
+    //   (A, {A, B, C, D, E, F})
+    //   (B, {A, B, C, D, E, F})
+    //   (C, {B})
+    //   (D, {A, B})
+    //   (E, {B, D, F})
+    //   (F, {B, E})
+    val links = lines.map{ s =>
+      val parts = s.split("\\s+")
+      (parts(0), parts(1))
+    }.distinct().groupByKey().cache()
+
+    // Output ranks
+    //   (A, 1)
+    //   (B, 1)
+    //   (C, 1)
+    //   (D, 1)
+    //   (E, 1)
+    //   (F, 1)
+    var ranks = links.mapValues(v => 1.0)
+
+    for (i <- 1 to iters) {
+      val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
+        val size = urls.size
+        urls.map(url => (url, rank / size))
+      }
+      ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
+    }
+
+    val output = ranks.collect()
+    output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
+
+    ctx.stop()
+  }
+}
