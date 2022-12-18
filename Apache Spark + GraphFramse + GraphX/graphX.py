94235b5f18820cc0fa575cdbb95c3b907f613c44
Divya Pandey <divya.pandey012430@gmail.com>
2022-12-18 11:05:14 -0800

    Add files via upload

---
+++ Apache Spark + GraphFramse + GraphX/graphX.py
@@ -0,0 +1,72 @@
+# Import PySpark
+import pyspark
+from pyspark.sql import SparkSession
+
+#Create SparkSession
+spark = SparkSession.builder.master("local[1]").appName("pysparkGraphX").getOrCreate()
+
+from graphframes import *
+
+# Recipe 9-1. Create GraphFrames
+#     person dataframe : id, Name, age
+personsDf = spark.read.csv('in/person.csv',header=True, inferSchema=True)
+
+# Create a "persons" SQL table from personsDF DataFrame
+personsDf.createOrReplaceTempView("persons")
+spark.sql("select * from persons").show()
+
+# relationship dataframe : src, dst, relation
+relationshipDf = spark.read.csv('in/relationship.csv',header=True, inferSchema=True)
+relationshipDf.createOrReplaceTempView("relationship")
+spark.sql("select * from relationship").show()
+
+# - Create a GraphFrame from both person and relationship dataframes
+#     >>> graph
+#     GraphFrame(v:[id: int, Name: string ... 1 more field], e:[src:
+#     int, dst: int ... 1 more field])
+# - A GraphFrame that contains v and e.
+#   + The v represents vertices and e represents edges.
+graph = GraphFrame(personsDf, relationshipDf)
+
+# - Degrees represent the number of edges that are connected to a vertex.
+#   + GraphFrame supports inDegrees and outDegrees.
+#     - inDegrees give you the number of incoming links to a vertex.
+#     - outDegrees give the number of outgoing edges from a node.
+# - Find all the edges connected to Andrew.
+graph.degrees.filter("id = 1").show()
+
+# Find the number of incoming links to Andrew
+graph.inDegrees.filter("id = 1").show()
+
+# Find the number of links coming out from Andrew using the outDegrees
+graph.outDegrees.filter("id = 1").show()
+
+# Recipe 9-2. Apply Triangle Counting in a GraphFrame
+# - Find how many triangle relationships the vertex is participating in
+personsTriangleCountDf = graph.triangleCount()
+personsTriangleCountDf.show()
+
+# Create a "personsTriangleCount" SQL table from the 
+# personsTriangleCountDf DataFrame
+personsTriangleCountDf.createOrReplaceTempView("personsTriangleCount")
+
+# Create a "personsMaxTriangleCount" SQL table from the
+# maxCountDf DataFrame
+maxCountDf = spark.sql("select max(count) as max_count from personsTriangleCount")
+maxCountDf.createOrReplaceTempView("personsMaxTriangleCount")
+
+spark.sql("select * from personsTriangleCount P JOIN (select * from personsMaxTriangleCount) M ON (M.max_count = P.count) ").show()
+
+# Recipe 9-3. Apply a PageRank Algorithm
+pageRank = graph.pageRank(resetProbability=0.20, maxIter=10)
+pageRank.vertices.printSchema()
+
+pageRank.vertices.orderBy("pagerank",ascending=False).show()
+
+pageRank.edges.orderBy("weight",ascending=False).show()
+
+# Recipe 9-4. Apply the Breadth First Algorithm
+graph.bfs(fromExpr = "Name='Bob'",toExpr = "Name='William'").show()
+
+graph.bfs(fromExpr = "age < 20", toExpr = "name = 'Rachel'").show()
+graph.bfs(fromExpr = "age < 20", toExpr = "name = 'Rachel'", edgeFilter = "relation != 'Son'").show()
